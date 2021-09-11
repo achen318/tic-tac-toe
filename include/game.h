ca181c348de9de8905715efac888096f17413f8e
Anthony Chen <50970130+achen318@users.noreply.github.com>
2021-09-11 12:40:33 -0400

    Add documentation to header files

--- include/game.h
+++ include/game.h
@@ -6,30 +6,86 @@
 class Game
 {
 public:
-    // Accessors
+    /**
+     * @returns A QList of QPushButton pointers.
+     */
     QList<QPushButton *> board();
+
+    /**
+     * @param index An integer value between 0 and 8.
+     * @returns The QPushButton text ('X', 'O', '\\0') that is at index in board().
+     */
     char box(int index);
 
+    /**
+     * @returns The current player ('X', 'O').
+     */
     char turn();
 
+    /**
+     * @returns Whether or not the game is over.
+     */
     bool game_over();
 
+    /**
+     * @returns Integer score of player X.
+     */
     int x_score();
+
+    /**
+     * @returns Integer score of player O.
+     */
     int o_score();
 
-    // Mutators
+    /**
+     * @brief Add a QPushButton pointer to board().
+     * 
+     * This method should only be called in the initial game setup.
+     * 
+     * @param box A pointer to a QPushButton.
+     */
     void append_to_board(QPushButton *box);
+
+    /**
+     * @brief Set the text for the box at the given index in board().
+     * 
+     * @param index An integer value between 0 and 8.
+     * @param value The char value (should only be 'X' or 'O') to be set in the box.
+     */
     void set_box(int index, char value);
 
+    /**
+     * @brief Alternate player_turn() between player X and O.
+     */
     void switch_turn();
 
+    /**
+     * @brief Set the game_over() value to be true or false.
+     * 
+     * @param game_state Whether or not the game is over.
+     */
     void set_game_over(bool game_state);
 
+    /**
+     * @brief Add 1 to x_score().
+     */
     void increment_x_score();
+
+    /**
+     * @brief Add 1 to o_score().
+     */
     void increment_o_score();
 
-    // Other member functions
+    /**
+     * @brief Clear the board and set player_turn() to 'X'.
+     */
     void NewGame();
+
+    /**
+     * @brief Get the Winner object
+     * 
+     * @returns The winning player ('X', 'O').
+     */
     char GetWinner();
 
 private:
--- include/tictactoe.h
+++ include/tictactoe.h
@@ -15,14 +15,29 @@ class TicTacToe : public QMainWindow
     Q_OBJECT
 
 public:
+    /**
+     * @brief Construct a new TicTacToe object.
+     * 
+     * @param parent A pointer to a QWidget parent.
+     */
     TicTacToe(QWidget *parent = nullptr);
+
+    /**
+     * @brief Destroy the TicTacToe object.
+     */
     ~TicTacToe();
 
 private:
     Ui::TicTacToe *ui;
 
 private slots:
+    /**
+     * @brief Logic for when a box emits a released() signal.
+     */
     void BoxPressed();
+    /**
+     * @brief Logic for when the new game button emits a released() signal.
+     */
     void NewGame();
 };
 
